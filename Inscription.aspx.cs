41d5b1b849ef5e8f65e31eaf9c03b781a96a96ca
fdelaney <delaneyfrancis@hotmail.com>
2014-04-14 20:55:16 -0400

    adding cryting

--- Inscription.aspx.cs
+++ Inscription.aspx.cs
@@ -2,6 +2,7 @@
 using System.Data.OleDb;
 using System.Configuration;
 using System.Web.UI.WebControls;
+using System.Security.Cryptography;
 
 public partial class Default2 : System.Web.UI.Page
 {
@@ -14,6 +15,26 @@ public partial class Default2 : System.Web.UI.Page
         if (this.IsValid)
         {
             string userName = txbPseudo.Text;
+
+            string mdp = txbMPasse.Text;  //TextBox du mot de passe
+            byte[] tabSels = new byte[24];  //Tableau pour le sel
+
+            RNGCryptoServiceProvider rdmSel = new RNGCryptoServiceProvider();
+
+            rdmSel.GetBytes(tabSels);  //Creation du sel aléatoire
+
+
+            Rfc2898DeriveBytes hash = new Rfc2898DeriveBytes(mdp, tabSels, 1000);
+
+            byte[] password = hash.GetBytes(24);  //Obtention du mot de passe crypté
+
+            //INSCRIPTION
+            //Store le mdp hashé
+            //Store le sel
+
+            //CONNECTION
+            //Hasher le mdp entré avec le sel de la bd
+            //Comparer le mdp hashé avec celui de la bd
             
             OleDbConnection connection = new OleDbConnection(ConfigurationManager.ConnectionStrings["GeneralDatabase"].ConnectionString);
             connection.Open();
@@ -24,22 +45,15 @@ public partial class Default2 : System.Web.UI.Page
 
             if ((int)command.ExecuteScalar() < 1)
             {
-                string image = "";
-                string passWord = txbMPasse.Text;
-                if (fileUpload.HasFile)
-                {
-                    image = userName + System.IO.Path.GetExtension(fileUpload.FileName);
-                    fileUpload.SaveAs(Server.MapPath("assets/image/" + image));
-                }
-               
+                string passWord = password.ToString();
+                string image = SaveAs(userName + System.IO.Path.GetExtension(fileUpload.FileName));
+
                 command = new OleDbCommand("INSERT INTO Utilisateurs VALUES (@userName,@password, @image)", connection);
                 command.Parameters.Add(new OleDbParameter("userName", userName) { OleDbType = OleDbType.VarChar, Size = 255 });
                 command.Parameters.Add(new OleDbParameter("password", passWord) { OleDbType = OleDbType.VarChar, Size = 255 });
                 command.Parameters.Add(new OleDbParameter("image", image) { OleDbType = OleDbType.VarChar, Size = 255 });
                 command.Prepare();
                 command.ExecuteNonQuery();
-
-
             }
 
             
