ba0b1577fca143011b0b9b294e03f4db18d2bb2e
Francois Chantal <geniousmouton@gmail.com>
2014-04-14 23:14:58 -0400

    changement d'avatar

--- changementDAvatar.aspx
+++ changementDAvatar.aspx
@@ -1,9 +1,50 @@
-﻿<%@ Page Title="Changement d'avatar" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="changementDAvatar.aspx.cs" Inherits="Default2" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="changementDAvatar.aspx.cs" Inherits="Default2" %>
 
-<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
-    
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+    <title>Changement d'avatar</title>
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="body" Runat="Server">
+<asp:Content ID="Content2" ContentPlaceHolderID="body" runat="Server">
 
+    <%if (Session["id"] == null)
+      { %>
+    <label>veullier vous connecter pour contiuer</label>
+    <asp:LinkButton
+        runat="server"
+        Text="Redirection"
+        PostBackUrl="~/Default.aspx"
+        CausesValidation="false"></asp:LinkButton>
+
+    <%}
+      else
+      { %>
+    <div>
+
+        <asp:Label Text="" runat="server" ID="lblsucces" CssClass="label-success" />
+
+        <asp:FileUpload
+            runat="server"
+            ID="fileUpload1" />
+        <asp:RequiredFieldValidator
+            runat="Server"
+            ControlToValidate="fileUpload1"
+            ErrorMessage="l'image est requise"
+            class="alert-danger">
+        </asp:RequiredFieldValidator>
+
+        <asp:CustomValidator
+            runat="server"
+            ID="imageValidator"
+            ErrorMessage="l'image n'est pas valide"
+            ControlToValidate="fileUpload1"
+            OnServerValidate="imageValidator_ServerValidate"
+            class="alert-danger">
+        </asp:CustomValidator>
+        <br />
+        <asp:Button
+            runat="server"
+            OnClick="btnImage_Click"
+            Text="Modifier l'avatar" />
+    </div>
+
+    <%} %>
 </asp:Content>
-
--- changementDAvatar.aspx.cs
+++ changementDAvatar.aspx.cs
@@ -1,14 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.OleDb;
 
 public partial class Default2 : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+    }
 
+    protected void imageValidator_ServerValidate(object source, System.Web.UI.WebControls.ServerValidateEventArgs args)
+    {
+        if (fileUpload1.HasFile)
+        {
+            try
+            {
+                System.Drawing.Image img = System.Drawing.Image.FromStream(fileUpload1.FileContent);
+                if (img.Height > 180 || img.Width > 180)
+                {
+                    args.IsValid = false;
+                }
+            }
+            catch
+            {
+                args.IsValid = false;
+            }
+        }
+        else
+        {
+            args.IsValid = false;
+        }
+    }
+
+    protected void btnImage_Click(object sender, EventArgs e)
+    {
+        if (this.IsValid)
+        {
+            string userName = (string)Session["id"];
+            OleDbConnection connection = new OleDbConnection(ConfigurationManager.ConnectionStrings["GeneralDatabase"].ConnectionString);
+            connection.Open();
+
+            string newimage = "";
+
+            newimage = userName + System.IO.Path.GetExtension(fileUpload1.FileName);
+            fileUpload1.SaveAs(Server.MapPath("assets/image/" + newimage));
+
+            OleDbCommand command = new OleDbCommand(" UPDATE Utilisateurs SET nom_fichier_avatar=@newImage WHERE nom_utilisateur = @userName;", connection);
+            command.Parameters.Add(new OleDbParameter("newImage", newimage) { OleDbType = OleDbType.VarChar, Size = 255 });
+            command.Parameters.Add(new OleDbParameter("userName", userName) { OleDbType = OleDbType.VarChar, Size = 255 });
+            command.Prepare();
+            command.ExecuteNonQuery();
+
+            lblsucces.Text = "Modification reussis!";
+        }
     }
 }
